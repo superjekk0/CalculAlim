2514f4ccb6cca6981eba61af1dab58be2f76da61
superjekk <superjekk@outlook.com>
2024-08-23 14:15:14 -0400

    FCT : Ajout de la base de données
    L'application a maintenant une base de données locale à utiliser afin de lire les aliments disponibles par défaut

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,16 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    val roomVersion = "2.6.1"
+
+    implementation("androidx.room:room-common:${roomVersion}");
+    implementation("androidx.room:room-rxjava2:${roomVersion}");
+    implementation("androidx.room:room-rxjava3:${roomVersion}");
+    implementation("androidx.room:room-guava:${roomVersion}");
+
+    annotationProcessor("androidx.room:room-compiler:${roomVersion}");
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
