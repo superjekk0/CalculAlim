0e6044de405e4f7ed4450d5c2f48db0caab4c5e6
CMandziuk (superjekk) <superjekk@outlook.com>
2024-10-23 13:39:37 -0400

    FCT : Choix d'aliments
    Il est maintenant possible de sélectionner le type d'aliment souhaité. Un menu radio offres les options disponibles.

--- app/src/main/java/org/mandziuk/calculalim/MainActivity.kt
+++ app/src/main/java/org/mandziuk/calculalim/MainActivity.kt
@@ -1,24 +1,32 @@
 package org.mandziuk.calculalim
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.FileUtils
 import android.util.Log
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.room.Room
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.dialog.MaterialDialogs
 import org.mandziuk.calculalim.databinding.ActivityMainBinding
 import org.mandziuk.calculalim.db.FoodDb
+import org.mandziuk.calculalim.db.models.FoodGroup
 import java.io.File
 import java.io.FileDescriptor
 import java.io.InputStream
 import java.nio.file.Files
+import java.util.ArrayList
+import java.util.Locale
 import kotlin.io.path.Path
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
 
     private lateinit var binding : ActivityMainBinding;
+    private lateinit var choixGroupes : List<FoodGroup>;
+    private var indexGroup : Long = 0L;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater);
@@ -32,12 +40,29 @@ class MainActivity : AppCompatActivity() {
             .build();
 
         val foodDao = db.getFoodDao();
+        choixGroupes = foodDao.getGroups();
         Log.i("EXEMPLE", foodDao.getGroups().toString());
 
         binding.choix.setOnClickListener {
-            val dialog = MaterialAlertDialogBuilder(applicationContext).create();
-            // TODO : Faire un layout de bouton radio pour les choix
-            dialog.setTitle("Choix");
+            val choix = choixGroupes.map { g ->
+                if (Locale.getDefault().displayLanguage.contains("fr", true))
+                    g.groupNameFr
+                else
+                    g.groupName }.toMutableList();
+            choix.add(0, getString(R.string.tousGroupes));
+            val builder = AlertDialog.Builder(this);
+
+            builder.setTitle("Choix").setSingleChoiceItems(choix.toTypedArray(), 0) { dialog, index ->
+                run {
+                    binding.choix.text = choix[index];
+                    indexGroup = index.toLong();
+                    dialog.dismiss();
+
+                }
+            }
+
+            val dialog = builder.create();
+            dialog.show();
         };
 
         binding.list.layoutManager = LinearLayoutManager(this);
