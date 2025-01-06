d05a1c6e1e7894f90cc5bd6ec034138bc11bd533
superjekk <superjekk@outlook.com>
2025-01-06 15:59:04 -0500

    FCT : Modification du dialogue
    L'interface du dialogue a été remaniée, avec des données associées aux différents composants. Les fonctionnalités d'enregistrement des données reste à faire.

--- app/src/main/java/org/mandziuk/calculalim/dialogs/AddMealDialog.kt
+++ app/src/main/java/org/mandziuk/calculalim/dialogs/AddMealDialog.kt
@@ -5,41 +5,54 @@ import android.view.LayoutInflater
 import androidx.appcompat.app.AlertDialog
 import androidx.core.widget.doOnTextChanged
 import org.mandziuk.calculalim.R
-import org.mandziuk.calculalim.activities.indexGroup
 import org.mandziuk.calculalim.databinding.DialogAddMealBinding
 import org.mandziuk.calculalim.db.dtos.FoodGroupDTO
 
 class AddMealDialog(context: Context, groupes: List<FoodGroupDTO>) : AlertDialog.Builder(context) {
-    var mealName: String? = "";
-    var mealWeight: Int? = 0;
-    var indexGroup: Long = 0L;
+    var mealName: String? = null;
+    var mealWeight: Int? = null;
+    var foodGroupId: Long = 0L;
+    var portionWeight: Int? = null;
+    var portionName: String? = null;
     private lateinit var binding : DialogAddMealBinding;
+    private lateinit var dialog: AlertDialog;
 
     init {
         initializeBinding();
         setView(binding.root);
 
-        setNegativeButton( "HELP"){
-            dialog, _ -> dialog.dismiss();
-        };
+        binding.positif.setOnClickListener {
+            var erreurActive = false;
+            if (groupes.find { it.foodGroupId == foodGroupId } == null){
+                binding.groupeAliment.error = context.getString(R.string.erreurGroupeAliment);
+                erreurActive = true;
+            }
 
-        setPositiveButton("OK"){
-            dialog, _ -> dialog.dismiss();
+            if (mealName == null || mealName == ""){
+                binding.nomAliment.error = context.getString(R.string.erreurNomAliment);
+                erreurActive = true;
+            }
+
+            if (!erreurActive){
+                dialog.dismiss();
+            }
         }
 
+        binding.negatif.setOnClickListener {
+            dialog.cancel();
+        }
 
         binding.groupeAliment.setOnClickListener { _  ->
             val groupDialog = AlertDialog.Builder(context);
-            groupDialog.setTitle(R.string.typeNourriture).setSingleChoiceItems(groupes.map { cg -> cg.groupName }.toTypedArray(), indexGroup.toInt()) { dialog, index ->
+            groupDialog.setTitle(R.string.typeNourriture).setSingleChoiceItems(groupes.map { cg -> cg.groupName }.toTypedArray(), foodGroupId.toInt()) { dialog, index ->
                 binding.groupeAliment.text = groupes[index].groupName;
-                indexGroup = index.toLong();
+                foodGroupId = groupes[index].foodGroupId;
                 dialog.dismiss();
+                binding.groupeAliment.error = null;
             }
 
             groupDialog.show();
         }
-
-        //dialogAddMealBinding.groupeAliment.getWindowVisibleDisplayFrame(Rect())
 
         binding.quantiteMasse.doOnTextChanged { text, _, _, _ ->
             mealWeight = when (text?.length) {
@@ -53,6 +66,24 @@ class AddMealDialog(context: Context, groupes: List<FoodGroupDTO>) : AlertDialog
         binding.nomAliment.doOnTextChanged { text, _, _, _ ->
             mealName = text?.toString();
         }
+
+        binding.nomPortion.doOnTextChanged { text, _, _, _ ->
+            portionName = text?.toString();
+        }
+
+        binding.quantitePortion.doOnTextChanged { text, _, _, _ ->
+            portionWeight = when (text?.length) {
+                null -> null;
+                0 -> null;
+                else ->
+                    text.toString().toInt();
+            }
+        }
+    }
+
+    override fun show(): AlertDialog {
+        this.dialog = super.show();
+        return dialog;
     }
 
     private fun initializeBinding(){
