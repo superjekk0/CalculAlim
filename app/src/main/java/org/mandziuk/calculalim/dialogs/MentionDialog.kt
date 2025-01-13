2cd1476f0c0e67375bb031afc5951d22fd7bfa7b
superjekk <superjekk@outlook.com>
2025-01-12 23:40:06 -0500

    BUGFIX : Accès au lien
    Le lien amène maintenant bien à la page de Santé-Canada

--- app/src/main/java/org/mandziuk/calculalim/dialogs/MentionDialog.kt
+++ app/src/main/java/org/mandziuk/calculalim/dialogs/MentionDialog.kt
@@ -2,9 +2,12 @@ package org.mandziuk.calculalim.dialogs
 
 import android.content.Context
 import android.text.SpannableString
+import android.text.method.LinkMovementMethod
+import android.text.method.MovementMethod
 import android.text.util.Linkify
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
+import androidx.core.text.method.LinkMovementMethodCompat
 import org.mandziuk.calculalim.R
 
 class MentionDialog(context: Context) : AlertDialog(context) {
@@ -15,6 +18,7 @@ class MentionDialog(context: Context) : AlertDialog(context) {
         text.text = spannableString;
         text.setPadding(20, 10, 20, 10);
         text.textSize = 18f;
+        text.movementMethod = LinkMovementMethod.getInstance();
         setView(text);
         setTitle(R.string.titreMentionsLegales);
         setButton(BUTTON_POSITIVE, context.getString(R.string.ok)) { dialog, _ -> dialog.dismiss() };
